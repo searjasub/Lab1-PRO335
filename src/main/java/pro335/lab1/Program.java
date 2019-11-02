aaa2480a59d962b37e78b5d746f06de97f2636e2
Pavdrew <pavloandrew@gmail.com>
2019-11-01 19:46:09 -0600

    Revert "Merge branch 'master' of C:\Users\Andrew Pav\Documents\Neumont Projects\Quarter 5\Persistence Project\Lab1-PRO335 with conflicts."
    This reverts commit c2000c68ce25cffde014931a5f38de27309a1879.

--- src/main/java/pro335/lab1/Program.java
+++ src/main/java/pro335/lab1/Program.java
@@ -1,41 +1,12 @@
 package pro335.lab1;
 
-import javax.xml.stream.XMLInputFactory;
-import java.util.ArrayList;
-import java.util.List;
-
-import java.sql.Connection;
+import pro335.lab1.controller.Driver;
 
 public class Program {
 
-    private List<Customer> customerList = new ArrayList<>();
-    private List<Orders> orderList = new ArrayList<>();
-    private List<OrderLines> orderLineList = new ArrayList<>();
-
-    private Connection connection;
-
     public static void main(String[] args) {
 
-        String path = "\\customers.xml";
-
-
-    }
-
-    private List<Customer> xmlCustomerParse(String filePath) {
-        return null;
-    }
-
-    private List<Orders> xmlOrdersParse(String filePath) {
-        return null;
-    }
-
-    private List<OrderLines> xmlOrderLinesParse(String filePath) {
-        return null;
-    }
-
-    private void databaseEntry(List<Customer> customerList, List<Orders> ordersList, List<OrderLines> orderLinesList) {
-
-    }
-    private void connectToDb() {
+        Driver driver = new Driver();
+        driver.run();
     }
 }
