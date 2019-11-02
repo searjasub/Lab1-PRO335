1b3395725f9f0038b1f82b1f9ec191d2a1d5f0ef
nmolstad <nashmolstad@gmail.com>
2019-11-01 19:07:11 -0600

    customer class

--- src/main/java/pro335/lab1/Customer.java
+++ src/main/java/pro335/lab1/Customer.java
@@ -2,4 +2,50 @@ package pro335.lab1;
 
 public class Customer {
 
+    private int customerId;
+    private String name;
+    private String email;
+    private int age;
+
+    public Customer() {
+    }
+
+    public Customer(int customerId, String name, String email, int age) {
+        setCustomerId(customerId);
+        setName(name);
+        setEmail(email);
+        setAge(age);
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
