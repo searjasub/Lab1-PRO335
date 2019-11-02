1784fc96477c76580f5515d739cf0faa0457c346
nmolstad <nashmolstad@gmail.com>
2019-11-01 19:33:52 -0600

    SQL Server dependency

--- src/main/java/pro335/lab1/Program.java
+++ src/main/java/pro335/lab1/Program.java
@@ -3,11 +3,15 @@ package pro335.lab1;
 import java.util.ArrayList;
 import java.util.List;
 
+import java.sql.Connection;
+
 public class Program {
 
     private List<Customer> customerList = new ArrayList<>();
     private List<Orders> orderList = new ArrayList<>();
     private List<OrderLines> orderLineList = new ArrayList<>();
+
+    private Connection connection;
 
     public static void main(String[] args) {
 
@@ -31,5 +35,9 @@ public class Program {
     private void databaseEntry(List<Customer> customerList, List<Orders> ordersList, List<OrderLines> orderLinesList) {
 
     }
+    private void connectToDb() {
+        try() {
 
+        }
+    }
 }
