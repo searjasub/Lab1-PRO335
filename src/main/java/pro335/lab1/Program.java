e900da2848de5a0dd5f80d7df544cf9fba477dec
Searjasub Lopez <searjasub.lopez@outlook.com>
2019-11-01 19:30:53 -0600

    update git ignore

--- src/main/java/pro335/lab1/Program.java
+++ src/main/java/pro335/lab1/Program.java
@@ -1,8 +1,18 @@
 package pro335.lab1;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Program {
 
+    private List<Customer> customerList = new ArrayList<>();
+    private List<Orders> orderList = new ArrayList<>();
+    private List<OrderLines> orderLineList = new ArrayList<>();
+
     public static void main(String[] args) {
+
+        String path = "\\customers.xml";
+
 
     }
 
