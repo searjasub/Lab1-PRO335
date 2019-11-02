58dd17b91c6bbe2ed0b36c8e689181faeb668120
Pavdrew <pavloandrew@gmail.com>
2019-11-01 19:29:18 -0600

    Order java object creation

--- src/main/java/pro335/lab1/Program.java
+++ src/main/java/pro335/lab1/Program.java
@@ -1,10 +1,27 @@
 package pro335.lab1;
 
+import java.util.List;
+
 public class Program {
 
     public static void main(String[] args) {
 
     }
 
+    private List<Customer> xmlCustomerParse(String filePath) {
+        return null;
+    }
+
+    private List<Orders> xmlOrdersParse(String filePath) {
+        return null;
+    }
+
+    private List<OrderLines> xmlOrderLinesParse(String filePath) {
+        return null;
+    }
+
+    private void databaseEntry(List<Customer> customerList, List<Orders> ordersList, List<OrderLines> orderLinesList) {
+
+    }
 
 }
