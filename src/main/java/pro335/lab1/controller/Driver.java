fd0ff0167e6a5b9c814761eb7cde3027d6dcf312
Searjasub Lopez <searjasub.lopez@outlook.com>
2019-11-05 18:09:32 -0700

    fix it!

--- src/main/java/pro335/lab1/controller/Driver.java
+++ src/main/java/pro335/lab1/controller/Driver.java
@@ -1,5 +1,6 @@
 package pro335.lab1.controller;
 
+import com.microsoft.sqlserver.jdbc.SQLServerDataTable;
 import pro335.lab1.model.Customer;
 import pro335.lab1.model.Order;
 import pro335.lab1.model.OrderLine;
@@ -56,8 +57,8 @@ public class Driver {
             System.out.println(orderLine.toString());
         }
 
-//        connectToDb();
-//        insertCustomersToDb(customerList);
+        connectToDb();
+        insertCustomersToDb(customerList);
     }
 
     private List<Customer> xmlCustomerParse(String filePath) {
@@ -210,13 +211,8 @@ public class Driver {
     private void connectToDb() {
         try {
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-
-        try {
-            connection = DriverManager.getConnection("jdbc://sqlserver://localhost;databaseName=customers;user=lab1;password=lab1");
-        } catch (SQLException e) {
+            connection = DriverManager.getConnection("jdbc:sqlserver://localhost","lab1", "lab1");
+        } catch (SQLException | ClassNotFoundException e) {
             e.printStackTrace();
         }
     }
